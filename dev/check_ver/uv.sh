3bb65e10260151c8289b9eae0f99ec8af6fbc46e
Paolo G. Giarrusso <paolo@skylabs-ai.com>
2026-01-09 15:09:18 +0100

    check_ver: Rename misnamed MAX

--- dev/check_ver/uv.sh
+++ dev/check_ver/uv.sh
@@ -6,7 +6,7 @@
 PROG="uv"
 URL="https://github.com/astral-sh/uv/blob/main/README.md"
 MIN="0.9.0"
-MAX="0.10.0"
+FIRST_UNSUPPORTED="0.10.0"
 
 print_ver() {
   uv self version --short | cut -d' ' -f1
