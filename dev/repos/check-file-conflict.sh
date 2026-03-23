3f46b4b4af40aac1345be006cbf961a78657e150
Rodolphe Lepigre <rodolphe.lepigre@skylabs-ai.com>
2026-03-23 17:39:42 +0100

    Script for detecting file conflicts.
    This is useful to avoid issues on case-insensitive file-systems,
    as is the default on macOS.

---
+++ dev/repos/check-file-conflict.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#
+# This script makes sure that no two files exist under the workspace that may
+# lead to a conflict on a case-insensitive file system (like on macOS).
+
+TEMP_FILE="$(mktemp)"
+
+make -j ls-files | sort -i | uniq -i -D > ${TEMP_FILE}
+make -j ls-files | sed 's|/[^/]\+$||' | sort -u | sort -i | uniq -i -D >> ${TEMP_FILE}
+
+NB_DUPS=$(wc -l < ${TEMP_FILE})
+
+if [[ ${NB_DUPS} -ne 0 ]]; then
+  echo "==== ${NB_DUPS} conflicts on case-insensitive file systems ===="
+  cat ${TEMP_FILE}
+  rm ${TEMP_FILE}
+  exit 1
+fi
+
+rm ${TEMP_FILE}
